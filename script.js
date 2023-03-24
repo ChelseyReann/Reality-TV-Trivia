001bdb8ec333aa98bb9ad9151ffd5e4eee61add0
ChelseyReann <chelseyreann@gmail.com>
2023-03-24 12:19:46 -0400

    updates nextQuestion function to take you to the final page when the questions are done

--- script.js
+++ script.js
@@ -1,47 +1,77 @@
-let start = document.querySelector(".start")
-let instructions = document.querySelector(".instructions")
- 
+let start = document.querySelector(".start");
+let instructions = document.querySelector(".instructions");
+let finalScreen = document.querySelector(".final");
+
 //question & options return an array
-let question = document.querySelectorAll(".question")
-let options = document.querySelectorAll(".options")
+let question = document.querySelectorAll(".question");
+let options = document.querySelectorAll(".options");
 
 //these variables store the current & previous question index in the question array
-let currentQuestion = 0
-let previousQuestion = 0
+let currentQuestion = 0;
+let previousQuestion = 0;
 
-let score = 0
-let answers = document.querySelectorAll(".answers")
+//creating a variable to set the starting score to 0
+let score = 0;
 
 //once the start button is clicked, the first question is shown on the page
-start.addEventListener("click", startGame)
-function startGame(){
-    question[currentQuestion].style.display = "block"
-    instructions.style.display = "none"
+start.addEventListener("click", startGame);
+function startGame() {
+  question[currentQuestion].style.display = "block";
+  instructions.style.display = "none";
 }
 
 //iterate over each option and add an event listener that will take you to the next question page
 options.forEach((option) => {
-    option.addEventListener("click", nextQuestion)
-})
+  option.addEventListener("click", nextQuestion);
+});
 
 //hides the previous question and allows the next question to be displayed
-function nextQuestion(){
-    previousQuestion = currentQuestion
-    currentQuestion++
-    question[previousQuestion].style.display = "none"
-    question[currentQuestion].style.display = "block"
+function nextQuestion() {
+  previousQuestion = currentQuestion
+  currentQuestion++
+  if (currentQuestion === question.length) {
+    question[previousQuestion].style.display = "none";
+    finalScreen.style.display = "block";
+  } else {
+    question[previousQuestion].style.display = "none";
+    question[currentQuestion].style.display = "block";
+  }
 }
 
 //iterate through each of the buttons to calculate score if the correct option has been clicked
-options.forEach((option) =>{
-    option.addEventListener("click", calculateScore)
-})
-function calculateScore (e){
-    console.log(e.target.classList.value)
-    if(e.target.classList.value === "options correct"){
-        score += 10
-        console.log(score)
-    } else {
-        console.log(score)
-    }
+options.forEach((option) => {
+  option.addEventListener("click", calculateScore);
+});
+function calculateScore(e) {
+  console.log(e.target.classList.value);
+  if (e.target.classList.value === "options correct") {
+    score += 10;
+    console.log(score);
+  } else {
+    console.log(score);
+  }
 }
+
+// if (score === 0){
+//     console.log("You're a Reality TV Show novice. Go watch some trash TV. Any trash TV. Please.")
+// } else if (score === 10){
+//     console.log("You're a Reality TV Show novice. Go watch some trash TV. Any trash TV. Please.")
+// } else if (score === 20){
+//     console.log("You're a Reality TV Show beginner. Please go binge watch Jersey Shore or Love is Blind and feel better about yourself.")
+// } else if (score === 30){
+//     console.log("You're a Reality TV Show beginner. Please go binge watch Jersey Shore or Love is Blind and feel better about yourself.")
+// } else if (score === 40){
+//     console.log("You're a Reality TV Show newcomer. Go watch some more trash TV. Please.")
+// } else if (score === 50){
+//     console.log("You're a Reality TV Show newcomer. Go watch some more trash TV. Please.")
+// } else if (score === 60){
+//     console.log("You're a Reality TV Show fan. Go watch some more trash TV. Please.")
+// } else if (score === 70){
+//     console.log("You're a Reality TV Show fan. Go watch some more trash TV. Please.")
+// } else if (score === 80){
+//     console.log("You're a Reality TV Show expert! Let's talk about out favorite shows together!")
+// } else if (score === 90){
+//     console.log("You're a Reality TV Show expert. Let's talk about our favorite shows together!")
+// } else if (score === 100) {
+//     console.log("You're a Reality TV Show connoisseur! Let's talk about our favorite shows together!")
+// }
