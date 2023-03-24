7e1cf5abae161589c808570a6f3afb31bf811260
ChelseyReann <chelseyreann@gmail.com>
2023-03-23 20:55:06 -0400

    updates css and adds first event listener

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 let start = document.querySelector(".start")
+let instructions = document.querySelector(".instructions")
+//question returns an array
+let question = document.querySelectorAll(".question")
+
+//these variables store the current & previous question index in the auestion array
+let currentQuestion = 0
+let previousQuestion = 0
+
+
+start.addEventListener("click", startGame)
+function startGame(){
+    question[currentQuestion].style.display = "block"
+    instructions.style.display = "none"
+}
