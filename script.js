937fc057a29b44ec796f4c81664a8e3664d80276
ChelseyReann <chelseyreann@gmail.com>
2023-03-23 21:26:40 -0400

    adds second event listener to the answer options

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 let start = document.querySelector(".start")
 let instructions = document.querySelector(".instructions")
-//question returns an array
+//question & options return an array
 let question = document.querySelectorAll(".question")
+let options = document.querySelectorAll(".options")
 
-//these variables store the current & previous question index in the auestion array
+//these variables store the current & previous question index in the question array
 let currentQuestion = 0
 let previousQuestion = 0
 
-
+//once the start button is clicked, the first question is shown on the page
 start.addEventListener("click", startGame)
 function startGame(){
     question[currentQuestion].style.display = "block"
     instructions.style.display = "none"
+}
+
+//iterate over each option and add an event listener that will take you to the next question page
+options.forEach((option) => {
+    option.addEventListener("click", nextQuestion)
+})
+
+function nextQuestion(e){
+    e.preventDefault()
+    previousQuestion = currentQuestion
+    currentQuestion++
+    console.log(question[currentQuestion])
+    question[previousQuestion].style.display = "none"
+    question[currentQuestion].style.disply = "block"
+
 }
