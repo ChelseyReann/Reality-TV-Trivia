1f7c21579d3a662a0816cfad04edec4397864423
ChelseyReann <chelseyreann@gmail.com>
2023-03-24 11:07:49 -0400

    adds thrid event listener to calculate score

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let start = document.querySelector(".start")
 let instructions = document.querySelector(".instructions")
+ 
 //question & options return an array
 let question = document.querySelectorAll(".question")
 let options = document.querySelectorAll(".options")
@@ -7,6 +8,9 @@ let options = document.querySelectorAll(".options")
 //these variables store the current & previous question index in the question array
 let currentQuestion = 0
 let previousQuestion = 0
+
+let score = 0
+let answers = document.querySelectorAll(".answers")
 
 //once the start button is clicked, the first question is shown on the page
 start.addEventListener("click", startGame)
@@ -20,10 +24,24 @@ options.forEach((option) => {
     option.addEventListener("click", nextQuestion)
 })
 
+//hides the previous question and allows the next question to be displayed
 function nextQuestion(){
     previousQuestion = currentQuestion
     currentQuestion++
     question[previousQuestion].style.display = "none"
     question[currentQuestion].style.display = "block"
+}
 
+//iterate through each of the buttons to calculate score if the correct option has been clicked
+options.forEach((option) =>{
+    option.addEventListener("click", calculateScore)
+})
+function calculateScore (e){
+    console.log(e.target.classList.value)
+    if(e.target.classList.value === "options correct"){
+        score += 10
+        console.log(score)
+    } else {
+        console.log(score)
+    }
 }
